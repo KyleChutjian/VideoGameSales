9883b9ec51535886c03f3b765d01a6299675739b
treckert <treckert@quinnipiac.edu>
2021-11-04 10:43:45 -0400

    test 2

--- main.py
+++ main.py
@@ -49,58 +49,65 @@ def setup_db(cur):
 
 
 def insert_data(cur):
-    cur.execute('USE roster_db')
-
-    fname = 'roster_data.json'
-
-    # Data structure as follows:
-    #   [
-    #   [ "Charley", "si110", 1 ],
-    #   [ "Mea", "si110", 0 ],
-
-    # open the file and read
-    str_data = open(fname).read()
-    # load the data in a json object
-    json_data = json.loads(str_data)
-
-    # json data is loaded in a pyton list
-    for entry in json_data:
-        name = entry[0]
-        title = entry[1]
-
-        print(name)
-        print(title)
-
-        # INSERT OR IGNORE satisfies the uniqueness constraint. the inserted data will be ignored if we try to add duplicates.
-        # works as both insert and update
-        cur.execute('''INSERT IGNORE INTO User (name)  
-            VALUES ( %s )''', (name))
-
-        # look up the primary key from inserted data.
-        cur.execute('SELECT id FROM User WHERE name = %s ', (name,))
-        user_id = cur.fetchone()[0]
-
-        # same technique is used to insert the title
-        cur.execute('''INSERT IGNORE INTO Course (title) 
-            VALUES ( %s )''', (title,))
-        cur.execute('SELECT id FROM Course WHERE title = %s ', (title,))
-        course_id = cur.fetchone()[0]
-
-        # insert both keys in the many to many connector table.
-        cur.execute('''INSERT IGNORE INTO Member
-            (user_id, course_id) VALUES ( %s, %s )''',
-                    (user_id, course_id))
 
 
-cnx = make_connection()
-cur = cnx.cursor()
-print("Starting Setup...")
-setup_db(cur)
-print("Finished Setup.")
-print("Starting Insert...")
-insert_data(cur)
-print("Finished Insert.")
-cur.close()
-cnx.commit()
-cnx.close()
-print("FINISHED")
+
+
+
+
+
+#     cur.execute('USE roster_db')
+#
+#     fname = 'roster_data.json'
+#
+#     # Data structure as follows:
+#     #   [
+#     #   [ "Charley", "si110", 1 ],
+#     #   [ "Mea", "si110", 0 ],
+#
+#     # open the file and read
+#     str_data = open(fname).read()
+#     # load the data in a json object
+#     json_data = json.loads(str_data)
+#
+#     # json data is loaded in a pyton list
+#     for entry in json_data:
+#         name = entry[0]
+#         title = entry[1]
+#
+#         print(name)
+#         print(title)
+#
+#         # INSERT OR IGNORE satisfies the uniqueness constraint. the inserted data will be ignored if we try to add duplicates.
+#         # works as both insert and update
+#         cur.execute('''INSERT IGNORE INTO User (name)
+#             VALUES ( %s )''', (name))
+#
+#         # look up the primary key from inserted data.
+#         cur.execute('SELECT id FROM User WHERE name = %s ', (name,))
+#         user_id = cur.fetchone()[0]
+#
+#         # same technique is used to insert the title
+#         cur.execute('''INSERT IGNORE INTO Course (title)
+#             VALUES ( %s )''', (title,))
+#         cur.execute('SELECT id FROM Course WHERE title = %s ', (title,))
+#         course_id = cur.fetchone()[0]
+#
+#         # insert both keys in the many to many connector table.
+#         cur.execute('''INSERT IGNORE INTO Member
+#             (user_id, course_id) VALUES ( %s, %s )''',
+#                     (user_id, course_id))
+#
+#
+# cnx = make_connection()
+# cur = cnx.cursor()
+# print("Starting Setup...")
+# setup_db(cur)
+# print("Finished Setup.")
+# print("Starting Insert...")
+# insert_data(cur)
+# print("Finished Insert.")
+# cur.close()
+# cnx.commit()
+# cnx.close()
+# print("FINISHED")
